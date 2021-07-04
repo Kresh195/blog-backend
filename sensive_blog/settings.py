a071d916ad7586ab2c865042304d30b886435f81
Kresh195 <KreshF5@mail.ru>
2021-07-04 17:12:16 +0700

    Added frontend

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -92,10 +92,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = os.getenv('STATIC_URL', '/static/')
+STATIC_URL = '/assets/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
-
+STATICFILES_DIRS = [
+    './static/'
+]
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
 STATIC_ROOT = os.getenv("STATIC_ROOT")
